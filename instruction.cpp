47f807f173d6ffb2eed8a7e50e566a6f3deda98e
Zayed_Oyshik <zayedoyshik1@gmail.com>
2022-05-10 16:46:07 +0600

    Basic Instruction

---
+++ instruction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using  namespace std;
+
+int main(){
+    cout<<"Welcome to the book-shop app demonstration.\n";
+    cout<<"You can add a book here by specifying its\n";
+    cout<<"\t-Title\n";
+    cout<<"\t-Author\n";
+    cout<<"\t-Price\n";
+    cout<<"\t-Piece\n\n";
+    cout<<"Then you can buy anyone of them or change their details.\n\n";
+    cout<<"If the entered title and Author name is wrong then,\n";
+    cout<<"It will go back to the menu and ask again for your choice.\n\n";
+    cout<<"Thanks for using.\n";
+    return 0;
+}
